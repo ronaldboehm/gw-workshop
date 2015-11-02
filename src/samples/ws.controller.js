c274a7447150830c2a534583071544ffb898eb05
ronaldboehm <github@f0ffff.net>
2015-11-02 11:08:35 +0100

    Controller ohne $scope

--- src/samples/ws.controller.js
+++ src/samples/ws.controller.js
@@ -5,8 +5,16 @@
     .module('workshop')
     .controller('workshopController', WorkshopController);
 
-    function WorkshopController($scope) {
-      $scope.greeting = 'Hallo Welt';
-      $scope.attendees = ['Christoph', 'Gregor', 'Philipp', 'Ronald', 'Susann'];      
+    function WorkshopController() {
+      // this -> Zugriff auf den Scope
+      // in der view dann "... as workshop"
+      //   <section class="sample" ng-controller="workshopController as workshop">
+      // und diesen Prefix verwenden:
+      //   {{workshop.greeting}}
+      // Vorteile:
+      // - keine Abhängigkeit von $Scope
+      // - in der view Möglichkeit der Unterscheidung bei mehreren Controllern
+      this.greeting = 'Hallo Welt';
+      this.attendees = ['Christoph', 'Gregor', 'Philipp', 'Ronald', 'Susann'];
     }
 }());
